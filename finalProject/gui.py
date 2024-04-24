2205ac0305022a9f9a9f18666a834e2815e6f016
DC Cline <cline.dc4@gmail.com>
2024-04-24 10:26:11 -0700

    progress: using my desktop now for a bit

--- finalProject/gui.py
+++ finalProject/gui.py
@@ -11,40 +11,40 @@ try:
         password=getpass("Enter password: "),
     ) as connection:
         print(connection)
+        cursor = connection.cursor()
+
+        #function for coffeeIngredientButton
+        def coffeeIngredientButton():
+            
+
+        #coffee gui
+        def coffeeGui():
+            coffeeWindow = Tk()
+            coffeeWindow.title('DCBCDB: Coffee')
+            coffeeWindow.geometry('500x300')
+
+            #dropdown for drinks
+            options = []
+            #list drink names and set options for the dropdown menu.
+            cursor.execute('SELECT drink_name FROM dcbcdb.CoffeeDrink;')
+            for x in cursor:
+                options.append(x[0])
+            default = StringVar(master=coffeeWindow)
+            default.set(options[0])
+            #dropdown menu for drink names
+            coffeeDropdown = OptionMenu(coffeeWindow, default, *options)
+            coffeeDropdown.place(x=50, y=50)
+            coffeeButton
+
+            coffeeWindow.mainloop()
+
+        #home gui
+        testWindow = Tk()
+        testWindow.title('DCBCDB')
+        testWindow.geometry('500x300')
+        testButton = Button(testWindow, text = 'Coffee', command = coffeeGui)
+        testButton.place(x = 50, y = 50)
+        testWindow.mainloop()
+
 except Error as e:
     print(e)
-
-cursor = connection.cursor()
-
-#testButtonFunction
-def testButtonFunction(string):
-    string ='[' + string + ']'
-    msg=messagebox.showinfo(testEntry.get(), string)
-#home gui
-testWindow = Tk()
-testWindow.title('DCBCDB')
-testWindow.geometry('500x300')
-
-
-#coffee gui
-def coffeeGui():
-    coffeeWindow = Tk()
-    coffeeWindow.title('DCBCDB: Coffee')
-    coffeeWindow.geometry('500x300')
-
-    #dropdown for drinks
-    options = []
-    cursor.execute('SELECT drink_name FROM dcbcdb.CoffeeDrink;')
-    for x in cursor:
-        options.append(x)
-    coffeeDropdown = OptionMenu(coffeeWindow, options)
-    coffeeDropdown.pack
-    coffeeWindow.mainloop()
-
-coffeeGui()
-buttonString ="test: please click here:"
-testEntry = Entry(width = 50)
-testEntry.place(x = 50, y = 80)
-testButton = Button(testWindow, text = buttonString, command = lambda: testButtonFunction(buttonString))
-testButton.place(x = 50, y = 50)
-testWindow.mainloop()
