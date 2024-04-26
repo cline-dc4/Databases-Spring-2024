47855c9fcd509a7086b9b0ee20b60255be2df772
DC Cline <cline.dc4@gmail.com>
2024-04-26 12:48:59 -0700

    scrap and rebuild(added todos)

--- finalProject/gui.py
+++ finalProject/gui.py
@@ -66,40 +66,15 @@ try:
             coffeeNameEntry.place(x=120,y=20)
             coffeePriceLabel.place(x=50,y=70)
             coffeePriceEntry.place(x=120,y=70)
-
-            #Ingredient Checkboxes:
-            #get the list of ingredient names
-            getIngredientsQuery = "SELECT ingredient_name, ingredient_id FROM dcbcdb.CoffeeIngredient"
-            ingredientList = []
-            ingredientIdList = []
-            cursor.execute(getIngredientsQuery)
-            #add the checkbutton for each ingredient in the database
-            for x in cursor:
-                ingredientList.append(x[0])
-            i = 0
-            #list to hold the variables changed by checking and unchecking the boxes
-            checkbuttonVariableList = []
-            #create checkbuttons
-            for name in ingredientList:
-                #create ID list
-                ingredientIdList.append(x[1])
-                checkbuttonVariableList.append(Variable(value=0))
-            for name in ingredientList:
-                Checkbutton(coffeeAddWindow, variable = checkbuttonVariableList[i], text = name).place(x=50, y=90+(20*i))
-                i = i + 1
-                print(checkbuttonVariableList)
-
             #button to run add function
             runInsertButton = Button(coffeeAddWindow, text = 'Add drink', command = lambda: coffeeAdd())
             runInsertButton.place(x=350, y=50)
-
+            
+            #instance variables for functions
+            name = coffeeNameEntry.get()
+            price = coffeePriceEntry.get()
             #function for the coffeeAddButton
             def coffeeAdd():
-                checkbuttonStateList = []
-                for x in checkbuttonVariableList:
-                    checkbuttonStateList.append(x.get())
-                name = coffeeNameEntry.get()
-                price = coffeePriceEntry.get()
                 #construct sql query to add a row
                 drinkSqlQuery = "INSERT INTO dcbcdb.CoffeeDrink \
                             VALUES \
@@ -116,24 +91,16 @@ try:
 
                 #connection.commit()
 
-                #add to the ingredient list using checkbuttons
-                cursor.execute(DrinkIdSqlQuery)
-                drink_id = 0
-                i = 0
-                #find the drink_id for the new drink name
-                for x in cursor:
-                    drink_id = x[0]
-                print(checkbuttonStateList[1])
-                for x in checkbuttonStateList:
-                    print(x)
-                    print(i)
-                    print(checkbuttonStateList[i])
-                    if (checkbuttonStateList[i] == 1):
-                        #set up the query to add to IngredientList
-                        addIngredientQuery = "INSERT INTO dcbcdb.IngredientList VALUES(" + str(drink_id) + " " + str(ingredientIdList[i].get()) + ")"
-                        print(addIngredientQuery)
-                        i = i + 1
-                coffeeAddWindow.mainloop()
+                #open a new menu for IngredientList additions (use dropdown menus)
+                def ingredientAdd():
+
+                    #find the drink_id
+                    cursor.execute(DrinkIdSqlQuery)
+                    drink_id = 0
+                    for x in cursor:
+                        drink_id = x[0]
+
+                #TODO: include compatability for a dropdown menu of ingredients(when button pressed, add a row with drink_id and ingredient_id for selected ingredient)
 
         #home gui
         mainWindow = Tk()
